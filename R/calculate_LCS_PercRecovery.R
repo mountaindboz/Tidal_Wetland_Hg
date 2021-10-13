e8dfddd4e11392afdd0fd3e1a38903fad4443bf0
Dave Bosworth <David.Bosworth@water.ca.gov>
2021-10-13 11:41:49 -0700

    Adding QC data and code to calculate average LCS percent recoveries for THg and OC

--- R/calculate_LCS_PercRecovery.R
+++ R/calculate_LCS_PercRecovery.R
@@ -7,6 +7,7 @@
 # Load packages
 library(tidyverse)
 library(readxl)
+library(lubridate)
 
 # Source functions
 source("R/global_funcs.R")
@@ -35,4 +36,25 @@ lcs_mehg_summ <- qa_samp_mlml %>%
 
 lcs_mehg_summ
 # Average LCS Percent Recovery value for MeHg: 99.0% for 68 samples
+
+
+# Import QA sample data for Hg and organic carbon samples analyzed by Bryte Lab
+# This file is already cleaned and has duplicates removed. There was no consistent way
+  # of doing this, so we deleted duplicate records manually in the file.
+qa_samp_bryte <- read_excel("data/Bryte Instrument QC Data by Date_Measure_Analyte Q_DBoz_MHead_20210930.xlsx")
+
+# Calculate minimum, maximum and average of LCS percent recovery for Hg and organic carbon samples
+lcs_hg_oc_summ <- qa_samp_bryte %>%
+  mutate(QCR_PERCENT_RECOVERY = QCR_PERCENT_RECOVERY * 100) %>%
+  group_by(ANL_ANALYTE_NAME) %>%
+  summarize(
+    N = n(),
+    min_per_r = min(QCR_PERCENT_RECOVERY),
+    max_per_r = max(QCR_PERCENT_RECOVERY),
+    avg_per_r = round(mean(QCR_PERCENT_RECOVERY), 1)
+  )
+
+lcs_hg_oc_summ
+# Average LCS Percent Recovery value for THg: 98.8% for 113 samples
+# Average LCS Percent Recovery value for Organic Carbon: 99.5% for 67 samples
 
